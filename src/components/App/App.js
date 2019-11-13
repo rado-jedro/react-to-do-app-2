3f0c6f030d5318692d268ccc4a2385c799e3c2f0
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-13 23:08:01 +0000

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,8 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
-        <List title='Things to do test' />
+        <List title={['Things to do ', <sup key='1'>soon!</sup>]}>
+        </List>
       </main>
     )
   }
