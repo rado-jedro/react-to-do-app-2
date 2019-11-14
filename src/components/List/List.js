18a7065a12c6964c15c23c1f73acffd683342454
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-14 00:08:11 +0000

    Add Column and column props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,13 +2,16 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
+import Column from '../Column/Column.js';
 
 
 
 class List extends React.Component {
+
     static propTypes = {
         title: PropTypes.node.isRequired,
         children: PropTypes.node,
+        image: PropTypes.text,
     }
     static defaultProps = {
         children: <p>I can do all the things!!!</p>,
@@ -17,11 +20,18 @@ class List extends React.Component {
     return (
       <section className={styles.component}>
         <h2 className={styles.subtitle}>
-            <Hero titleText={this.props.title} />
+            <Hero titleText={this.props.title} image= {this.props.image} />
             <div className={styles.description}>
                 {this.props.children}
             </div>
+        
         </h2>
+        <div className={styles.columns}>
+            <Column columnTitle ={'Animals'} />
+            <Column columnTitle ={'Plants'} />
+            <Column columnTitle = {'Minerals'}/>
+        </div>
+        
       </section>
     )
   }
