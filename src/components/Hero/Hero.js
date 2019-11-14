fdad6e99e61c8a64939df98b59142f0331295be5
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-14 00:26:41 +0000

    Add ReactHtmlParser

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Hero.scss'
 import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
+
 
 
 const Hero = props => (
     <header className={styles.component}>
-      <h2 className={styles.title}>{props.titleText}</h2>
-      <img className={styles.image} src={props.image}></img>
+        <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
+        <img className={styles.image} src={props.image}></img>
     </header>
   );
   
@@ -15,5 +17,3 @@ const Hero = props => (
   };
 
   export default Hero;
-
-  //src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"
