0ebf37a600d469244fa19ed821183af673db5268
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-14 00:08:30 +0000

    Add image props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,10 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
-        <List title={['Things to do ', <sup key='1'>soon!</sup>]}>
+        <List title={['Things to do ', <sup>soon!</sup>]}
+        image={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}
+        >
+          
         </List>
       </main>
     )
