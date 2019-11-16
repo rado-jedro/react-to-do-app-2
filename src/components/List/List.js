098ff8bc7310668ca380d3bf8a89457f92d5db6a
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-16 00:49:39 +0000

    Add redux migration

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,18 +2,17 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column.js';
+//import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
-import Creator from '../Creator/Creator.js';
+//import Creator from '../Creator/Creator.js';
+import ReactHtmlParser from 'react-html-parser';
+
 
 
 
 
 
 class List extends React.Component {
-    state = {
-      columns: this.props.columns || [],
-    }
 
     static propTypes = {
       title: PropTypes.node.isRequired,
@@ -26,32 +25,19 @@ class List extends React.Component {
 
     }
 
-    addColumn(title){
-      this.setState(state => (
-        {
-          columns: [
-            ...state.columns,
-            {
-              key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-              title,
-              icon: 'list-alt',
-              cards: [],
-            },
-          ],
-        }
-      ));
-    }
-
     render() {
+      const {title, image, description} = this.props;
+
       return (
         <section className={styles.component}>
           <h2 className={styles.subtitle}>
-            <Hero titleText={this.props.title} image= {this.props.image} />
+            <Hero titleText={title} image={image} />
             <div className={styles.description}>
-              {this.props.description}
+              {ReactHtmlParser(description)}
             </div>
         
           </h2>
+          {/*
           <div className={styles.columns}>
             {this.state.columns.map(({key, ...columnProps}) => (
               <Column key={key} {...columnProps} />
@@ -61,7 +47,7 @@ class List extends React.Component {
           <div className={styles.creator}>
             <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
           </div>
-        
+            */}
         </section>
       );
     }
