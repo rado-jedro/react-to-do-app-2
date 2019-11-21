638ba1154d7163aead564f1520ff3817cc737b51
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-21 23:51:55 +0000

    Update Info.js

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Container from '../Container/Container';
+import Hero from '../Hero/Hero';
 
 const Info = () => (
   <Container>
-    <h2>Info</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <Hero titleText= 'test text' image = 'https://images.pexels.com/photos/2255441/pexels-photo-2255441.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' />
+      
   </Container>
 );
 
