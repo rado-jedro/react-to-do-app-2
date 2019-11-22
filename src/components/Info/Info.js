e5e46a132da2782d49915513852fc6238d3bfcaf
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-22 23:20:52 +0000

    Add infoData to read from dataStore.js

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Container from '../Container/Container';
 import Hero from '../Hero/Hero';
+import {infoData} from '../../data/dataStore';
 
 const Info = () => (
   <Container>
-    <Hero titleText= 'test text' image = 'https://images.pexels.com/photos/2255441/pexels-photo-2255441.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' />
-      
+    <Hero titleText={infoData.title} image = {infoData.image}/>
+    <p>{infoData.description}</p>
   </Container>
 );
 
