daa868f3ebc9006c2b915912424fcf99fb52041e
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-23 22:31:16 +0000

    Add Search to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import styles from './Header.scss';
 import Container from '../Container/Container';
 import Icon from '../Icon/Icon';
 import { headerIcon } from '../../data/dataStore';
+import Search from '../Search/Search';
 
 
 
@@ -18,6 +19,7 @@ class Header extends React.Component {
             <Link className={styles.logo} to='/'>
               <Icon name= {headerIcon.icon}/>
             </Link>
+            <Search />
             <nav>
               <NavLink exact to='/' activeClassName='active'>Home</NavLink>
               <NavLink exact to='/info' activeClassName='active'>Info</NavLink>
