9ba71b04e30402a2834cc5d22880443d7e909d98
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-25 22:47:37 +0000

    Create SearchResults.js

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import {settings} from '../../data/dataStore';
+//import styles from './SearchResults.scss';
+
+
+
+class SearchResults extends React.Component {
+    
+    static PropTypes = {
+      cards: PropTypes.array,
+      lists : PropTypes.array,
+    }
+}
+
+
+export default SearchResults;
