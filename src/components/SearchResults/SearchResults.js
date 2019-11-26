afa842723fb242929eebbb4801367ea32916fc43
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-26 23:02:17 +0000

    Add render()

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import {settings} from '../../data/dataStore';
-//import styles from './SearchResults.scss';
-
+import styles from './SearchResults.scss';
+import Container from '../Container/Container';
+import Card from '../Card/Card';
 
 
 class SearchResults extends React.Component {
     
-    static PropTypes = {
+    static propTypes = {
       cards: PropTypes.array,
       lists : PropTypes.array,
     }
-}
 
+    render() {
+      const {cards} = this.props;
+      return (
+        <section>
+          <Container>
+            <div className={styles.columns}>
+              {cards.map(cardsData => (
+                <Card key={cardsData.id} {...cardsData} />
+              ))}
+            </div>  
+          </Container>
+        </section>
+      );
+    }
+}
 
 export default SearchResults;
